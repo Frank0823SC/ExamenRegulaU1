23dad7c38622f183108f577945e36b6ded8c8bbb
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2021-01-18 17:18:40 -0600

    Update Main.java

--- Exa/Main.java
+++ Exa/Main.java
@@ -1,1 +1,11 @@
-
+package Exa;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        Scanner leer = new Scanner(System.in);
+        int num_elementos;
+        
+        System.out.println("Digite el número de elementos: ");
+        num_elementos = leer.nextInt();
+    }
+}
