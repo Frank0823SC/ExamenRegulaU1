02e95d8f6a957e6de1d15b28bad3228bec86759a
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2021-01-18 17:30:58 -0600

    Update Main.java

--- Exa/Main.java
+++ Exa/Main.java
@@ -2,10 +2,17 @@ package Exa;
 import java.util.Scanner;
 public class Main {
     public static void main(String[]args){
-        Scanner leer = new Scanner(System.in);
         int num_elementos;
-        
+        Lista lista = new Lista();
+        String dato;
+        Scanner user = new Scanner(System.in);
+
         System.out.println("Digite el número de elementos: ");
-        num_elementos = leer.nextInt();
+        num_elementos=user.nextInt();
+        lista.crearArreglo(num_elementos);
+
+        for (int i = 0; i < num_elementos ; i++) {
+            System.out.println("Digite el dato "+(i+1)+" :");
+        }
     }
 }
