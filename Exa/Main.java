c807ccbe7f44b517bec64b84769c886e41bb19d5
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2021-01-18 17:41:18 -0600

    Update Main.java

--- Exa/Main.java
+++ Exa/Main.java
@@ -5,6 +5,7 @@ public class Main {
         int num_elementos;
         Lista lista = new Lista();
         String dato;
+        int respuesta;
         Scanner user = new Scanner(System.in);
 
         System.out.println("Digite el número de elementos: ");
@@ -13,6 +14,17 @@ public class Main {
 
         for (int i = 0; i < num_elementos ; i++) {
             System.out.println("Digite el dato "+(i+1)+" :");
+            dato=user.next();
+            lista.llenarLista(dato);
         }
+        Lista.mostrarLista();
+        
+        do {
+            System.out.println("\n");
+            System.out.println("¿Desea ingresar un dato más?");
+            System.out.println("1.- Si   2.- No");
+            respuesta = user.nextInt();
+        }while (respuesta!=2);
+
     }
 }
