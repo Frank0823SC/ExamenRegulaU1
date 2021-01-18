367cb47409495db99bca086e2662c98c1b25f699
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2021-01-18 17:32:09 -0600

    Update Lista.java

--- Exa/Lista.java
+++ Exa/Lista.java
@@ -1,1 +1,13 @@
+package Exa;
+public class Lista {
+        String Lista[];
+    public void crearArreglo(int num_elementos){
+        Lista = new String[num_elementos];
+    }
 
+    public void llenarLista(String dato){
+        for (int i = 0; i <= Lista.length-1 ; i++) {
+            Lista[i]=dato;
+        } 
+    }
+}
